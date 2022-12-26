838ff55b58a8e2bcdd2c46842ead63f56f88ce64
cheboluvenkatesh <cheboluvenkatesh04@gmail.com>
2022-12-26 11:40:20 +0530

    values

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "ec2-sample" {
   source  = "app.terraform.io/VenkateshChebolu/ec2-sample/aws"
   version = "1.0.1"
   # insert required variables here
-  ami = "0ceecbb0f30a902a6"
+  ami = "ami-0b5eea76982371e91"
   instance_type="t2.micro"
   key_name="mykey"
   security_group_ids=["sg-08077ef0bf51076a8"]
